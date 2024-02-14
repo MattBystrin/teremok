35f81b5567ef1318e5c5cebdd1eefb38e850c084
Matthew Bystrin <dev.mbstr@gmail.com>
2024-02-14 13:01:29 +0300

    tests: finished database test

--- tests/database.sh
+++ tests/database.sh
@@ -16,9 +16,9 @@ init() {
 
 room_types() {
 	# Insert room type
-	local id=$(psql --csv -c "insert into room_types(capacity, cost, label) values (1, 4, 'room for predators') returning id" | tail +2 | head -1)
+	id=$(psql --csv -c "insert into room_types(capacity, cost, label) values (1, 4, 'room for predators') returning id" | tail +2 | head -1)
 	psql -c "insert into rooms(type) values (${id})"
-	local value=$(psql --csv -c "select id from rooms where type=${id}" | tail +2)
+	value=$(psql --csv -c "select id from rooms where type=${id}" | tail +2)
 	# Assert correct insertion
 	[[ -n $value ]] || return 1
 	# Now remove room type and watch for the changes
@@ -44,6 +44,7 @@ book_records() {
 	psql -c "delete from users where id = $user"
 	book=$(psql --csv -c "select id from book_records where uid = $user" | tail +2)
 	[[ -z $book ]] || return 1
+	psql -c "delete from species where id = $specie"
 	return 0
 }
 
@@ -58,22 +59,31 @@ clean_tasks() {
 	values ('2024-01-24', false, $room, $user) returning id")
 	task=$(psql --csv -c "select id from clean_tasks where user_id = $user" | tail +2)
 	[[ -n $task ]] || return 1
-	psql -c "delete from users where email = 'testemployee@mail.ru'"
+	psql -c "delete from users where id = $user"
 	task=$(psql --csv -c "select id from clean_tasks where user_id = $user" | tail +2)
 	[[ -z $task ]] || return 1
+	psql -c "delete from species where id = $specie"
 	return 0
 }
 
-users() {
-	#Check tasks, notifications
-	# psql -c "select * from users"
+furniture () {
+	room_type=$(psql --csv -c "insert into room_types(capacity, cost, label) values (1, 4, 'room for predators') returning id" | tail +2 | head -1)
+	room=$(psql --csv -c "insert into rooms(type) values ($room_type) returning id" | tail +2 | head -1)
+	f_type=$(psql --csv -c "insert into furniture_types(cost, name) values (1, 'bed') returning id" | tail +2 | head -1)
+	f=$(psql --csv -c "insert into furniture(state, type, room) values (5, $f_type, $room) returning id" | tail +2 | head -1)
+	f_chk=$(psql --csv -c "select id from furniture where type = $f_type")
+	[[ -n $f_chk ]] || return 1
+	[[ $f == $f_chk ]] || return 1
+	psql -c "delete from furniture_types where id = $f_type"
+	f_chk=$(psql --csv -c "select id from furniture where type = $f_type")
+	[[ -z $f_chk ]] || return 1
 	return 0
 }
 
-for tcase in book_records room_types clean_tasks users
+for tcase in book_records room_types clean_tasks furniture
 do
 	result="${green}Passed${clean}"
-	$tcase > /dev/null
+	$tcase > /dev/null 2>&1
 	if [[ $? != 0 ]]; then
 		result="${red}Failed${clean}"
 	fi
